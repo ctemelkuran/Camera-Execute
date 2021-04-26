007df008b3f10a4076c0aeae05dedc3701f538d9
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-04-26 19:01:19 +0300

    revitoprthop deleted

--- Camera Execute/Command.cs
+++ Camera Execute/Command.cs
@@ -29,55 +29,7 @@ namespace Camera_Execute
 
             changeCameraForm.Show();
 
-            #region RevitOrthoCamera
-            // Save current 3D view
-            var view3D = doc.ActiveView as View3D;
-
-            if (view3D == null || view3D.IsPerspective)
-            {
-                TaskDialog ts = new TaskDialog("Incorrect View selected")
-                {
-                    MainContent = "Please, select 3D Orthographic view."
-                };
-
-                ts.Show();
-                return Result.Succeeded;
-            }
-
-            // Get viewOrientation3D
-            ViewOrientation3D viewOrientation3D = view3D.GetOrientation();
-            ForwardDirection = viewOrientation3D.ForwardDirection;
-            UpDirection = viewOrientation3D.UpDirection;
-            var rightDirection = ForwardDirection.CrossProduct(UpDirection);
-            EyePosition = viewOrientation3D.EyePosition;
-
-            IList<UIView> views = uidoc.GetOpenUIViews();
-            UIView currentView = views.FirstOrDefault(t => t.ViewId == view3D.Id);
-
-            //Corners of the active UI view
-            if (currentView == null)
-            {
-                return Result.Succeeded;
-            }
-
-            IList<XYZ> corners = currentView.GetZoomCorners();
-            XYZ corner1 = corners[0];
-            XYZ corner2 = corners[1];
-
-            //center of the UI view
-            double x = (corner1.X + corner2.X) / 2;
-            double y = (corner1.Y + corner2.Y) / 2;
-            double z = (corner1.Z + corner2.Z) / 2;
-            XYZ viewCenter = new XYZ(x, y, z);
-            EyePosition = viewCenter;
-
-            // Calculate diagonal vector
-            XYZ diagVector = corner1 - corner2;
-
-            double height = 0.5 * Math.Abs(diagVector.DotProduct(UpDirection));
-            double width = 0.5 * Math.Abs(diagVector.DotProduct(rightDirection));
-            Scale = Math.Min(height, width);
-            #endregion
+        
 
 
             //bool isOpen = false;
